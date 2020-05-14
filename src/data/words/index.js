b43ed048583812da58b505f02c53b359881be4f8
BobbyAdamson <bobbyadamson1@gmail.com>
2020-05-13 22:03:43 -0400

    add relationship words to flashcards

--- src/data/words/index.js
+++ src/data/words/index.js
@@ -2,12 +2,23 @@ import countries from "./countries";
 import occupations from "./occupations";
 import general from "./general";
 import newwords from "./newwords";
+import family from "./family";
+import relationships from "./relationships";
 
-export default [...countries, ...general, ...occupations, ...newwords];
+export default [
+  ...countries,
+  ...family,
+  ...general,
+  ...occupations,
+  ...relationships,
+  ...newwords,
+];
 
 export const grouped = {
   countries,
+  family,
   general,
   newwords,
   occupations,
+  relationships,
 };
